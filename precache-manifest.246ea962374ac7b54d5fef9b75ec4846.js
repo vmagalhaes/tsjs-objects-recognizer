398f580f03751eb7d1e39af67835da99b1f4cdbf
victor.magalhaes <victor.magalhaes@nexxera.com>
2019-06-27 17:24:51 -0300

    Updates

---
+++ precache-manifest.246ea962374ac7b54d5fef9b75ec4846.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ade239b84faa215292631680eb2ccca",
+    "url": "/tsjs-objects-recognizer/index.html"
+  },
+  {
+    "revision": "b8123a2bfe47211616fc",
+    "url": "/tsjs-objects-recognizer/static/css/main.57262ed2.chunk.css"
+  },
+  {
+    "revision": "5d9791630463a9324570",
+    "url": "/tsjs-objects-recognizer/static/js/2.b3b7a8bd.chunk.js"
+  },
+  {
+    "revision": "b8123a2bfe47211616fc",
+    "url": "/tsjs-objects-recognizer/static/js/main.3c77b949.chunk.js"
+  },
+  {
+    "revision": "49e1ccbc5e1c5d03e112",
+    "url": "/tsjs-objects-recognizer/static/js/runtime~main.58ec8d5f.js"
+  }
+]);
